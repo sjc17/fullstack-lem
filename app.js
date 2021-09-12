39d7f095a8ada876aac016306ef2c75cc0c5f770
Steven Choi <sjc@ualberta.ca>
2021-09-11 22:43:21 -0600

    Updated package.json scripts

--- app.js
+++ app.js
@@ -13,17 +13,26 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/public')));
+
+let serveFolder;
+if (process.env.NODE_ENV === 'production') {
+  serveFolder = 'client/dist';
+} else {
+  serveFolder = 'client/public';
+}
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`serveFolder: ${serveFolder}`);
+app.use(express.static(path.join(__dirname, serveFolder)));
 
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
