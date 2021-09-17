05554956977f3ec53228b1f1dbd8b6d454083487
Steven Choi <sjc@ualberta.ca>
2021-09-16 21:45:07 -0600

    Renamed env variables to avoid overlapping with existing PORT variable

--- config.js
+++ config.js
@@ -5,18 +5,18 @@ config.NODE_ENV = process.env.NODE_ENV;
 
 // Will be testing both the cloud DB and local DB here
 if (config.NODE_ENV == 'production') {
-    config.HOST = process.env.HOST;
-    config.PORT = process.env.PORT;
-    config.USER = process.env.USER;
-    config.PASSWORD = process.env.PASSWORD;
-    config.DATABASE = process.env.DATABASE;
+    config.HOST = process.env.DBHOST;
+    config.PORT = process.env.DBPORT;
+    config.USER = process.env.DBUSER;
+    config.PASSWORD = process.env.DBPASSWORD;
+    config.DATABASE = process.env.DBDATABASE;
     config.CA_CERT = process.env.CA_CERT;
 } else {
-    config.HOST = process.env.DEV_HOST;
-    config.PORT = process.env.DEV_PORT;
-    config.USER = process.env.DEV_USER;
-    config.PASSWORD = process.env.DEV_PASSWORD;
-    config.DATABASE = process.env.DEV_DATABASE;
+    config.HOST = process.env.DBDEV_HOST;
+    config.PORT = process.env.DBDEV_PORT;
+    config.USER = process.env.DBDEV_USER;
+    config.PASSWORD = process.env.DBDEV_PASSWORD;
+    config.DATABASE = process.env.DBDEV_DATABASE;
 }
 
 module.exports = config;
