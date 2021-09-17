2cfb29304892aed68e48f4afb06a1f7867f0d3dc
Steven Choi <sjc@ualberta.ca>
2021-09-16 19:38:07 -0600

    Pulling in CA cert from environment variable

--- database/db.js
+++ database/db.js
@@ -4,7 +4,8 @@ const { Pool, Client } = require('pg');
 const pool =
   process.env.NODE_ENV == 'production'
     ? new Pool({
-        connectionString: process.env.DATABASE_URL
+        connectionString: process.env.DATABASE_URL,
+        ca: process.env.CA_CERT
       })
     : new Pool();
 
