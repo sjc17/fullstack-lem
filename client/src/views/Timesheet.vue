4c56b3e12491c1824e0037e8f6196d5c1275e0d0
Steven Choi <sjc@ualberta.ca>
2021-12-25 15:37:05 -0700

    LEM Form mostly complete. Need to show running total, which will require refactoring the objects within the lemRows arrays (add rate & total properties).

--- client/src/views/Timesheet.vue
+++ client/src/views/Timesheet.vue
@@ -69,15 +69,21 @@
       </div>
     </div>
     <!-- LEM Rows -->
-    <div class="row">
+    <div class="row my-1">
+      <div class="col">Work Order</div>
       <div class="col">Item Code</div>
       <div class="col">Description</div>
-      <div class="col">Work Order</div>
       <div class="col">Quantity</div>
+      <div class="col">Rate</div>
+      <div class="col">Total</div>
+      <div class="col"></div>
     </div>
-    <div v-for="row in lemRows" :key="lemRows.findIndex(() => row)" class="row">
+    <div v-for="row in lemRows" :key="row.localId" class="lemRow row my-1 py-1">
+      <div class="col col-input">
+        <input type="text" name="" id="" v-model="row.workOrder" />
+      </div>
       <div class="col">
-        <select name="" :id="'row-'+lemRows.findIndex(() => row)">
+        <select name="" v-model="row.itemCode">
           <option
             v-for="item in lemItems"
             :key="item.itemcode"
@@ -88,14 +94,82 @@
         </select>
       </div>
       <div class="col">
-        <!-- Need to figure out how to get the selected item code and then display the description here -->
-        {{  }}
+        <!-- If item code is selected, lookup description of work item. Otherwise, use blank. -->
+        {{
+          !row.itemCode
+            ? ''
+            : this.lemItems.find((element) => element.itemcode == row.itemCode)
+                .description
+        }}
       </div>
-      <div class="col">{{ row.workOrder }}</div>
-      <div class="col">{{ row.quantity }}</div>
+      <div class="col col-input">
+        <input type="number" name="" id="" v-model="row.quantity" step="0.5" />
+      </div>
+      <div class="col">
+        <!-- If item code is selected, lookup rate. Otherwise, use 0. -->
+        {{
+          !row.itemCode
+            ? 0
+            : this.lemItems.find((element) => element.itemcode == row.itemCode)
+                .rate
+        }}
+      </div>
+      <div class="col">
+        <!-- If item code is selected, calculate total. Otherwise, use 0. -->
+        {{
+          !row.itemCode
+            ? 0
+            : new Intl.NumberFormat(undefined, {
+                style: 'currency',
+                currency: 'USD',
+              }).format(
+                parseFloat(
+                  this.lemItems
+                    .find((element) => element.itemcode == row.itemCode)
+                    .rate.replace('$', '')
+                ) * row.quantity
+              )
+        }}
+      </div>
+      <div class="col">
+        <i class="bi bi-trash" @click="deleteRow(row.localId)"></i>
+      </div>
+    </div>
+    <div class="row my-1">
+      <div class="col">Totals:</div>
+      <div class="col"></div>
+      <div class="col"></div>
+      <div class="col">
+        {{ this.lemRows.reduce((sum, row) => sum + row.quantity, 0) }}
+      </div>
+      <div class="col"></div>
+      <div class="col"></div>
+      <div class="col">
+      </div>
+    </div>
+    <div class="d-flex justify-content-center my-3">
+      <i class="bi bi-plus-square" @click="addRow"></i>
     </div>
   </form>
 </template>
+
+<style scoped>
+/* Padding causes the column divs containing inputs to overflow. This will address it. */
+.col-input {
+  padding-left: 0;
+  padding-right: 0;
+}
+/* LEM Rows styling */
+.lemRow {
+  border-top: solid rgba(175, 175, 175) 1px;
+  border-top: solid rgba(175, 175, 175) 1px;
+}
+/* Show icons as clickable */
+.bi:hover {
+  opacity: 50%;
+  cursor: pointer;
+}
+</style>
 
 <script>
 // import Alert from '../components/Alert.vue';
@@ -123,13 +197,14 @@ export default {
       selectedCompany: {},
       lemRows: [
         {
-          itemCode: '',
+          localId: 0,
           workOrder: '',
-          quantity: '',
-          comment: '',
+          itemCode: null,
+          quantity: 0,
         },
       ],
       lemItems: [],
+      counter: 1,
     };
   },
   methods: {
@@ -141,11 +216,27 @@ export default {
     async refreshPOs() {
       this.purchaseOrders = await getPurchaseOrders(this.selectedCompany.id);
     },
+    // Add LEM Row
+    addRow() {
+      // Append new row to the row array. We identify each row with a local ID to ensure
+      // no identity issues when deleting rows. Rows are tracked in the counter variable.
+      this.lemRows.push({
+        localId: this.counter++,
+        workOrder: '',
+        itemCode: null,
+        quantity: 0,
+      });
+    },
+    deleteRow(rowId) {
+      this.lemRows.pop((row) => row.localId == rowId);
+    },
   },
   async created() {
+    // Populate companies dropdown
     this.refreshCompanies();
+
+    // Get all LEM Item codes
     this.lemItems = await getLemItems();
-    console.log(this.lemItems[0]);
   },
 };
 </script>
